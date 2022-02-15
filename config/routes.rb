83368b5d44d2d85ce8a295f74743d21b284fe47b
Andy Maleh <andy.am@gmail.com>
2022-02-15 18:27:50 -0500

    Configured routes with Rails engine

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount Glimmer::Engine => "/glimmer" # add on top
   resources :welcomes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'welcomes#index'
 end
